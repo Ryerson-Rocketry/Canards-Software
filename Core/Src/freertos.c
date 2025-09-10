c131ac07c3bba3814b10244ac3c55fbb8c410ba7
etthann <110430934+etthann@users.noreply.github.com.>
2025-09-10 17:49:07 -0400

    Feat: Initialize STM32F446RE Project

---
+++ Core/Src/freertos.c
@@ -0,0 +1,30 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+// Task handle and attributes
+osThreadId_t defaultTaskHandle;
+
+const osThreadAttr_t defaultTask_attributes = {
+    .name = "defaultTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
+};
+
+void StartDefaultTask(void *argument);
+
+// FreeRTOS initialization
+void MX_FREERTOS_Init(void)
+{
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+}
+
+// Default task function
+void StartDefaultTask(void *argument)
+{
+  for (;;)
+  {
+    osDelay(1);
+  }
+}
