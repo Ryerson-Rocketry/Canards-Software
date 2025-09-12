bc210d4d56da2a8d464484ffb2d06b09f8b896c6
etthann <110430934+etthann@users.noreply.github.com.>
2025-09-11 21:00:56 -0400

    Feat: Created Madgwick Filter, Helpers and Task to compute orientation
    	- Created Madgwick Filter
    	- Created Madgwick math helpers
    	- Created Thread to compute orientation
    	- Created Struct for Madgwick filter variables
    	- Added Madgwick.c to build file

--- Core/Inc/madgwick.h
+++ Core/Inc/madgwick.h
@@ -1,313 +1,52 @@
-#include "math.h"
+#ifndef MADGWICK_H
+#define MADGWICK_H
 
-/*
- *  Perform the Quaternian Conjugate formula
- *  param: float q[4], input quaternian
- *  param: float out[4], array to hold quaternian conjugate
- *  return: array that holds the conjugate of the input
- */
-void quatConjugate(float q[4], float out[4])
+#include <math.h>
+
+typedef struct
 {
-    out[0] = q[0];
-    for (int i = 1; i < 4; i++)
-    {
-        out[i] = -1 * q[i];
-    }
-}
-
-void normalizeQuat(float q[4], float out[4])
-{
-    float norm = sqrtf(powf(q[0], 2) + powf(q[1], 2) + powf(q[2], 2) + powf(q[3], 2));
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = q[i] / norm;
-    }
-}
-
-/*
- *  Perform Quaternian Product between two Quaternians
- *  param: float a [4], quaternian one
- *  param: float b [4], quaternian two
- *  param: float out [3], output array
- *  return: (1x4 Matrix) quaternian but the matrix is transposed
- */
-void quatProduct(float a[4], float b[4], float out[4])
-{
-    out[0] = a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3];
-    out[1] = a[0] * b[1] + a[1] * b[0] + a[2] * b[3] - a[3] * b[2];
-    out[2] = a[0] * b[2] - a[1] * b[3] + a[2] * b[0] + a[3] * b[1];
-    out[3] = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0];
-}
-
-/*
- *  Convert Quaternian to Euler Angles
- *  param: float q [4], quaternian
- *  param: float out [3], output array
- *  return: [yaw, pitch, roll]
- */
-void quat2EulerAng(float q[4], float out[3])
-{
-    out[0] = atan2f(2 * q[1] * q[2] - 2 * q[0] * q[3], 2 * q[0] * q[0] + 2 * q[1] * q[1] - 1);
-    out[1] = -asinf(2 * q[1] * q[3] + 2 * q[0] * q[2]);
-    out[2] = atan2f(2 * q[2] * q[3] - 2 * q[0] * q[1], 2 * q[0] * q[0] + 2 * q[3] * q[3] - 1);
-}
-
-void scalarMulQuat(float q[4], float scalar, float out[4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = scalar * q[i];
-    }
-}
-
-void accelQuatDeriv(float accel[4], float q[4], float out[4])
-{
-    float temp[4];
-    scalarMulQuat(q, 0.5f, temp);
-    quatProduct(temp, accel, out);
-}
-
-void accelQuat(float q[4], float quatDeriv[4], float samplingTime, float out[4])
-{
-    float temp[4];
-    scalarMulQuat(quatDeriv, samplingTime, temp);
-
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = q[i] + temp[i];
-    }
-}
-
-void computeReferenceMag2EarthFrame(float q[4], float mag[4], float quatDeriv[4], float out[4])
-{
-    float temp[4];
-    quatProduct(q, mag, temp);
-    quatProduct(temp, quatDeriv, out);
-
-    out[0] = 0;
-    out[1] = sqrtf(out[1] * out[1] + out[2] * out[2]);
-    out[2] = 0;
-}
-
-void calcMagObjectiveFunc(float earthMagInc[4], float mag[4], float q[4], float out[3])
-{
-    out[0] = 2 * earthMagInc[1] * (0.5f - q[2] * q[2] - q[3] * q[3]) + 2 * earthMagInc[3] * (q[1] * q[3] - q[0] * q[2]) - mag[1];
-    out[1] = 2 * earthMagInc[1] * (q[1] * q[2] - q[0] * q[3]) + 2 * earthMagInc[3] * (q[0] * q[1] + q[2] * q[3]) - mag[2];
-    out[2] = 2 * earthMagInc[1] * (q[0] * q[2] + q[1] * q[3]) + 2 * earthMagInc[3] * (0.5f - q[1] * q[1] - q[2] * q[2]) - mag[3];
-}
-
-void calcMagJacobianMat(float q[4], float b[3], float J[3][4])
-{
-    float bx = b[0];
-    float bz = b[2];
-    float q1 = q[0];
-    float q2 = q[1];
-    float q3 = q[2];
-    float q4 = q[3];
-
-    J[0][0] = -2 * bz * q3;
-    J[0][1] = 2 * bz * q4;
-    J[0][2] = -4 * bx * q3 - 2 * bz * q1;
-    J[0][3] = -4 * bx * q4 + 2 * bz * q2;
-
-    J[1][0] = -2 * bx * q4 + 2 * bz * q2;
-    J[1][1] = 2 * bx * q3 + 2 * bz * q1;
-    J[1][2] = 2 * bx * q2 + 2 * bz * q4;
-    J[1][3] = -2 * bx * q1 + 2 * bz * q3;
-
-    J[2][0] = 2 * bx * q3;
-    J[2][1] = 2 * bx * q4 - 4 * bz * q2;
-    J[2][2] = 2 * bx * q1 - 4 * bz * q3;
-    J[2][3] = 2 * bx * q2;
-}
-
-void calcAccelObjFunc(float q[4], float accel[4], float out[3])
-{
-    float ax = accel[1];
-    float ay = accel[2];
-    float az = accel[3];
-
-    out[0] = 2 * (q[1] * q[3] - q[0] * q[2]) - ax;
-    out[1] = 2 * (q[0] * q[1] + q[2] * q[3]) - ay;
-    out[2] = 2 * (0.5f - q[1] * q[1] - q[2] * q[2]) - az;
-}
-
-void calcAccelJacobianMat(float q[4], float J[3][4])
-{
-    J[0][0] = -2 * q[2];
-    J[0][1] = 2 * q[3];
-    J[0][2] = -2 * q[0];
-    J[0][3] = 2 * q[1];
-
-    J[1][0] = 2 * q[1];
-    J[1][1] = 2 * q[0];
-    J[1][2] = 2 * q[3];
-    J[1][3] = 2 * q[2];
-
-    J[2][0] = 0;
-    J[2][1] = -4 * q[1];
-    J[2][2] = -4 * q[2];
-    J[2][3] = 0;
-}
-
-// shi I need to make this a 4x3 mat T
-void mat4x3Transpose(float q[4][3], float out[3][4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            out[j][i] = q[i][j];
-        }
-    }
-}
-
-void magAndAccelObjFunc(float gravObjFunc[3], float magObjFunc[3], float out[6])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        out[i] = gravObjFunc[i];
-        out[i + 3] = magObjFunc[i];
-    }
-}
-
-void magAndAccelJacobT(float gravJacobian[4][3], float magJacobian[4][3], float out[4][6])
-{
-    // transpose the Jacobian gravity matrix
-    float gravJacobT[3][4];
-    mat4x3Transpose(gravJacobian, gravJacobT);
-
-    // transpose the Jacobian magnetometer matrix
-    float magJacobT[3][4];
-    mat4x3Transpose(magJacobian, magJacobT);
-
-    float temp[6][4];
-
-    // Matrix stack to make a 6x4 matrix
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            temp[j][i] = gravJacobT[i][j];
-            temp[j + 3][i] = gravJacobian[i][j];
-        }
-    }
-
-    // Transpose the matrix to make a 4x6 matrix
-    for (int i = 0; i < 6; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            out[j][i] = temp[i][j];
-        }
-    }
-}
-
-void gradientObjFunc(float magAndAccelJMatT[4][6], float magAndAccelObj[6], float out[4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = 0;
-        for (int j = 0; j < 6; j++)
-        {
-            out[i] += magAndAccelJMatT[i][j] * magAndAccelObj[j];
-        }
-    }
-}
-
-void calcStepSize(float augmentationOfStepSize, float quatDeriv[4], float samplingSize, float out)
-{
-    if (augmentationOfStepSize < 1)
-    {
-        return;
-    }
-
-    float norm = sqrtf(powf(quatDeriv[0], 2) + powf(quatDeriv[1], 2) + powf(quatDeriv[2], 2) + powf(quatDeriv[3], 2));
-
-    out = augmentationOfStepSize * norm * samplingSize;
-}
-
-void gradientDescentQuat(float q[4], float stepSize, float gradient[4], float out[4])
-{
-
-    float temp[4];
-    float normalizedGradient[4];
-
-    normalizeQuat(gradient, normalizedGradient);
-    scalarMulQuat(normalizedGradient, stepSize, temp);
-
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = q[i] - temp[i];
-    }
-}
-
-void directionOfErrorQuat(float gradient[4], float out[4])
-{
-    normalizeQuat(gradient, out);
-}
-
-void estimatedQuatDeriv(float accelQuatDerivative[4], const float beta, float direcOfErrorQuat[4], float out[4])
-{
-    float temp[4];
-    scalarMulQuat(direcOfErrorQuat, beta, temp);
-
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = accelQuatDerivative[i] - temp[i];
-    }
-}
-
-void getGyroError(float q[4], float direcOfErrorQuat[4], float out[4])
-{
-    float temp[4];
-    scalarMulQuat(q, 2, temp);
-    quatProduct(temp, direcOfErrorQuat, out);
-}
-
-void getGyroBias(float gyroError[4], float samplingSize, float integralGain, float out[4])
-{
-    for (int i = 0; i < 4; ++i)
-    {
-        out[i] = integralGain * gyroError[i] * samplingSize;
-    }
-}
-
-void getCompensatedGyroMeas(float accel[4], float gyroBias[4], float out[4])
-{
-    for (int i = 0; i < 4; i++)
-    {
-        out[i] = accel[i] - gyroBias[i];
-    }
-}
-
-struct madgwick
-{
-    // input data, sensor frames
     float mag_sensor[4];
     float accel_sensor[4];
     float gyro_sensor[4];
-
-    // variables that can be tuned
+    float compensatedGyroMeasurements[4];
     float beta;
-    float integralGain; // zeta
-
-    // bias
-    float gyroBias[3];
-
-    // time step
+    float integralGain;
+    float augmentationOfStepSize;
+    float gyroBias[4];
     float dt;
-
-    // quaternians
-    float orientation[4];
+    float estOrientation[4];
     float direcErrorQuat[4];
-    float quatDeriv[4];
-    float gradidentQuat[4];
-
-    // Earth frames
+    float quatEstDeriv[4];
     float gravity[4];
-    float earthMagField[4];
-
-    // output of Euler angles
     float euler[3];
-};
+} Madgwick;
+
+// update function
+void updateMadgwick(Madgwick *madgwick);
+
+// Math helpers
+void quatConjugate(float q[4], float out[4]);
+void normalizeQuat(float q[4], float out[4]);
+void quatProduct(float a[4], float b[4], float out[4]);
+void quat2EulerAng(float q[4], float out[3]);
+void scalarMulQuat(float q[4], float scalar, float out[4]);
+void calcAccelQuatDeriv(float accel[4], float q[4], float out[4]);
+void calcAccelQuat(float q[4], float accelQuatDeriv[4], float samplingTime, float out[4]);
+void computeReferenceMag2EarthFrame(float q[4], float mag[4], float quatDeriv[4], float out[4]);
+void calcMagObjectiveFunc(float earthMagInc[4], float mag[4], float q[4], float out[3]);
+void calcMagJacobianMat(float q[4], float b[3], float J[3][4]);
+void calcAccelObjFunc(float q[4], float accel[4], float out[3]);
+void calcAccelJacobianMat(float q[4], float J[3][4]);
+void mat3x4Transpose(float q[3][4], float out[4][3]);
+void magAndAccelObjFunc(float fg[3], float fb[3], float out[6]);
+void magAndAccelJacobT(float gravJacobian[3][4], float magJacobian[3][4], float out[4][6]);
+void gradientObjFunc(float magAndAccelJMatT[4][6], float magAndAccelObj[6], float out[4]);
+void calcStepSize(float augmentationOfStepSize, float quatDeriv[4], float samplingSize, float *out);
+void calcGradientDescentQuat(float q[4], float stepSize, float gradient[4], float out[4]);
+void directionOfErrorQuat(float gradient[4], float out[4]);
+void estimatedQuatDeriv(float accelQuatDerivative[4], const float beta, float direcOfErrorQuat[4], float out[4]);
+void getGyroError(float q[4], float direcOfErrorQuat[4], float out[4]);
+void getGyroBias(float gyroError[4], float samplingSize, float integralGain, float out[4]);
+void getCompensatedGyroMeas(float accel[4], float gyroBias[4], float out[4]);
+
+#endif
