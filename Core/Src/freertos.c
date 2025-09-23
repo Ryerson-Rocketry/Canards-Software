f59054fe3672cb548b8aa8db58e799252ff15108
Ethan Ieong <110430934+etthann@users.noreply.github.com>
2025-09-23 00:39:16 -0400

    Merge feat/watchdog to implement watchdog and added back .ioc (#4)
    * Chore: Initalized Watchdog
    
    * Feat: Implemented Watchdog func and thread
    
    * Feat: Added ioc file for people to easily run project and watchdog

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -2,9 +2,11 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
+#include "wwdg.h"
 
 // Task handle and attributes
 osThreadId_t defaultTaskHandle;
+osThreadId_t refreshWatchdogHandle;
 
 const osThreadAttr_t defaultTask_attributes = {
     .name = "defaultTask",
@@ -12,12 +14,20 @@ const osThreadAttr_t defaultTask_attributes = {
     .priority = (osPriority_t)osPriorityNormal,
 };
 
+const osThreadAttr_t refereshWatchdog_Attributes = {
+    .name = "refershWatchdog",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal7,
+};
+
 void StartDefaultTask(void *argument);
+void RefreshWatchdog(void *argument);
 
 // FreeRTOS initialization
 void MX_FREERTOS_Init(void)
 {
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  refreshWatchdogHandle = osThreadNew(RefreshWatchdog, NULL, &refereshWatchdog_Attributes);
 }
 
 // Default task function
@@ -27,5 +37,24 @@ void StartDefaultTask(void *argument)
   {
     osDelay(1);
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  }
+}
+
+void RefreshWatchdog(void *argument)
+{
+
+  for (;;)
+  {
+
+    //  formula for watchdog tiemr: (1/fhclk) * 4096 * Nwwdg_prescaler * (Nrefresh -Nwindow)
+    osDelay(10);
+
+    // Toggle LED to see if on
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
+    if (HAL_WWDG_Refresh(&hwwdg) != HAL_OK)
+    {
+      Error_Handler();
+    }
   }
 }
