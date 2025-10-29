a4871de33f1b50589126d05d1c017f37612f3a9a
etthann <110430934+etthann@users.noreply.github.com.>
2025-10-29 11:22:20 -0400

    Fix: Remove magnetometer code as we are switching to I2C

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -6,16 +6,9 @@
 #include "usart.h"
 #include "string.h"
 #include "stdio.h"
-#include "mmc5983ma.h"
-
-#define GPIO_PIN_MAG_INT GPIO_PIN_MAG_INT_VAL
-
-// Semaphores
-SemaphoreHandle_t xMagDataReadySemaphore;
 
 // Task handle and attributes
 osThreadId_t defaultTaskHandle;
-osThreadId_t retrieveOrientationTaskHandle;
 
 const osThreadAttr_t defaultTask_attributes = {
     .name = "defaultTask",
@@ -23,20 +16,12 @@ const osThreadAttr_t defaultTask_attributes = {
     .priority = (osPriority_t)osPriorityNormal,
 };
 
-const osThreadAttr_t retrieveOrientation_attributes = {
-    .name = "getOrientation",
-    .stack_size = 128 * 4,
-    .priority = (osPriority_t)osPriorityRealtime,
-};
-
 void StartDefaultTask();
-void calculateOrientation();
 
 // FreeRTOS initialization
 void MX_FREERTOS_Init(void)
 {
 
-  mmc5983maInit();
   osDelay(1);
 
   xMagDataReadySemaphore = xSemaphoreCreateBinary();
@@ -52,42 +37,5 @@ void StartDefaultTask()
   {
     osDelay(1);
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-  }
-}
-
-
-void calculateOrientation()
-{
-
-  // needs to be changed to input it into Madgwick filter
-
-  float magData[4];
-
-  char uartBuf[100];
-
-  for (;;)
-  {
-    mmc5983maReadMagnetometer(xMagDataReadySemaphore, magData);
-
-    snprintf(uartBuf, sizeof(uartBuf),
-             "Mag: %.2f, %.2f, %.2f\r\n",
-             magData[1], magData[2], magData[3]);
-    HAL_UART_Transmit(&huart2, (uint8_t *)uartBuf, strlen(uartBuf), HAL_MAX_DELAY);
-
-    osDelay(1);
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-  }
-}
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-  // flag to see if there is a higher priority task
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  if (GPIO_Pin == GPIO_PIN_MAG_INT)
-  {
-    // if there is a higher priority task, xHigherPriorityTaskWoken becomes true
-    xSemaphoreGiveFromISR(xMagDataReadySemaphore, &xHigherPriorityTaskWoken);
-    // if higher priority task, switch to it after interrupt is done
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
   }
 }
