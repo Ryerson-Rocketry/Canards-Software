f85f075c3a53e2fec759e4ef39bbb1cb7b1f6371
etthann <110430934+etthann@users.noreply.github.com.>
2026-01-02 00:56:14 -0500

    Initialized FATFS and created script to R/W on SD card with HAL

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -4,9 +4,17 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "Drivers/ms5611.h"
+#include "ff.h"
+#include <string.h>
 
 // Task handle and attributes
 osThreadId_t defaultTaskHandle;
+
+// FatFs variables
+FATFS SDFatFS;
+FIL SDFile;
+UINT byteswritten;
+char SDPath[4] = "0:";
 
 const osThreadAttr_t defaultTask_attributes = {
     .name = "defaultTask",
@@ -28,6 +36,27 @@ void StartDefaultTask(void *argument)
   for (;;)
   {
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
+    /* 1. Mount the card */
+    if (f_mount(&SDFatFS, (TCHAR const *)SDPath, 1) == FR_OK)
+    {
+
+      /* 2. Open file for writing */
+      if (f_open(&SDFile, "DONE.TXT", FA_CREATE_ALWAYS | FA_WRITE) == FR_OK)
+      {
+
+        /* 3. Write your victory message */
+        char *msg = "Project Finished! HAL + SDIO is working.";
+        f_write(&SDFile, msg, strlen(msg), (void *)&byteswritten);
+
+        /* 4. Close the file */
+        f_close(&SDFile);
+
+        /* Success! Turn on the built-in LED (usually PA5) */
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+      }
+    }
+
     osDelay(10);
   }
 }
